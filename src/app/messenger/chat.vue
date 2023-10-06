050b7398828c2fd9f0315360f3a44058e861a5cd
Pavelavl <sfdrus2004@gmail.com>
2023-10-06 19:23:51 +0300

    IQSD-1052. Раскрытие картинки в виджете

--- src/app/messenger/chat.vue
+++ src/app/messenger/chat.vue
@@ -384,10 +384,105 @@
       }
     }
 
+.backdrop {
+  background: black;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 4;
+  opacity: 0.7;
+}
+
+.pending {
+  z-index: 5;
+  position: fixed;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-size: 18px;
+  color: white;
+  height: 100%;
+
+  .modal_img {
+    height: 100%;
+    display: flex;
+
+    img {
+      object-fit: contain;
+    }
+  }
+
+  .modal_img_footer {
+    padding: 25px;
+  }
+
+  .modal_img_header {
+    padding: 15px;
+    display: flex;
+    border-bottom: 1px gray solid;
+
+    .modal_img_header-icon {
+      padding: 10px;
+
+      svg {
+        fill: white;
+        width: 30px;
+        height: 30px;
+        transition: 0.3s ease;
+        opacity: 60%;
+        cursor: pointer;
+
+        &:hover {
+          opacity: 100%;
+        }
+      }
+    }
+
+    .modal_img_header-title {
+      margin-left: 10px;
+      display: flex;
+      flex-flow: column;
+      justify-content: space-around;
+      width: 100%;
+
+      .modal_img_header-title_date {
+        opacity: 0.6;
+        font-size: 16px;
+      }
+    }
+  }
+}
 </style>
 
 <template lang="pug">
     .messages#list
+
+        .backdrop(v-if="showImageModal")
+        .pending(v-if="showImageModal")
+            .modal_img_header
+                .modal_img_header-icon
+                    svg(@click="closeModalImg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512")
+                      path(d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z")
+                .modal_img_header-title
+                    span(v-if="modalImageMsg.Author === 'user'") {{ modalImageMsg.User.DisplayName }}
+                    span(v-if="modalImageMsg.Author === 'client'") {{ modalImageMsg.Client.Name }}
+                    span.modal_img_header-title_date {{ modalImageMsg.CreatedAt | humanDateTime }}
+                .modal_img_header-icon
+                    svg(@click="downloadModalImg(modalImageMsg)" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512")
+                      path(d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z")
+
+            .modal_img
+                img(:src="modalImageMsg.File.ThumbnailURL")
+
+            .modal_img_footer(v-if="modalImageMsg.Text")
+                span {{ modalImageMsg.Text }}
+
         .group(v-for="group in groups" v-bind:class="{ client: group.ClientId, user: group.UserId }" )
             .date(v-if="group.IsNewDay")
                 span.title {{ group.LastMessage.CreatedAt | humanDate }}
@@ -423,10 +518,17 @@
                           v-for="action of replyMsg.Actions", @click.prevent="trySendMessage(action.Title, action.Payload, action.URL)" ) {{ action.Title }}
                       div(v-else-if="replyMsg.File && replyMsg.File.Type == 'image'")
                         a.image(
-                          v-bind:href="replyMsg.File.URL",
+                          v-if="docWidth > 1024",
+                          v-bind:href="msg.File.URL",
                           target="_blank",
-                          @click="clickFile(replyMsg, $event)")
-                          img.bubble(:src="replyMsg.File.ThumbnailURL")
+                          @click="clickFile(msg, $event)"
+                        )
+                          img.bubble(:src="msg.File.ThumbnailURL", :class="{ first: index === 0, last: index === group.Messages.length - 1 }")
+                        .image(
+                          v-else-if="docWidth <= 1024",
+                          @click="clickFileImage(msg, $event)"
+                        )
+                          img.bubble(:src="msg.File.ThumbnailURL", :class="{ first: index === 0, last: index === group.Messages.length - 1 }")
                         button.img-button(v-if="replyMsg.Payload === 'carousel' || replyMsg.Payload === 'card'",
                           v-for="action of replyMsg.Actions") {{ action.Title }}
                       a.message_file(v-else-if="replyMsg.File && replyMsg.File.Type == 'file'"
@@ -459,9 +561,16 @@
                           v-for="action of msg.Actions", @click.prevent="trySendMessage(action.Title, action.Payload, action.URL)" ) {{ action.Title }}
                       div(v-else-if="msg.File && msg.File.Type == 'image'")
                         a.image(
-                        v-bind:href="msg.File.URL",
-                        target="_blank",
-                        @click="clickFile(msg, $event)")
+                          v-if="docWidth > 1024",
+                          v-bind:href="msg.File.URL",
+                          target="_blank",
+                          @click="clickFile(msg, $event)"
+                        )
+                          img.bubble(:src="msg.File.ThumbnailURL", :class="{ first: index === 0, last: index === group.Messages.length - 1 }")
+                        .image(
+                          v-else-if="docWidth <= 1024",
+                          @click="clickFileImage(msg, $event)"
+                        )
                           img.bubble(:src="msg.File.ThumbnailURL", :class="{ first: index === 0, last: index === group.Messages.length - 1 }")
                         div.img-caption
                           span {{ msg.Text }}
@@ -524,6 +633,7 @@ import rating from './rating.vue';
 import linkifyString from 'linkify-string';
 import client from '../../client';
 import inforequest from './info-request.vue';
+import { humanDateTime } from '../../lib/filters';
 
 export default {
   components: { inforequest, avatar, rating },
@@ -536,12 +646,15 @@ export default {
     rating: Object,
     client: Object,
     channel: String,
+    docWidth: Number,
   },
 
   data: function () {
     return {
       swipeRange: 100,
-      audioMessagePlayingId: null
+      audioMessagePlayingId: null,
+      showImageModal: false,
+      modalImageMsg: null,
     }
   },
 
@@ -550,6 +663,7 @@ export default {
   },
 
   methods: {
+    humanDateTime,
     scrollToLastMessage() {
       const el = $('#chat');
 
@@ -780,6 +894,24 @@ export default {
 
         event.preventDefault();
         this.$emit("click-file", file);
+    },
+
+    clickFileImage(msg, event) {
+      if (this.docWidth <= 1024) {
+        this.showImageModal = true;
+        this.modalImageMsg = msg;
+      }
+    },
+
+    closeModalImg() {
+      this.showImageModal = false;
+      this.modalImageMsg = null;
+    },
+
+    downloadModalImg(msg) {
+      let file = msg.File;
+
+      this.$emit("download-file", file);
     },
 
     clickLink(url, event, text) {
