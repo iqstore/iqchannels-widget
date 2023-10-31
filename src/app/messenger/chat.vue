7079a049a4a1bb3b6b4d0442739bb9f79fd920e6
Pavelavl <sfdrus2004@gmail.com>
2023-10-31 18:44:39 +0300

    IQSD-1052. fixes

--- src/app/messenger/chat.vue
+++ src/app/messenger/chat.vue
@@ -465,8 +465,9 @@
                     span(v-if="modalImageMsg.Author === 'user'") {{ modalImageMsg.User.DisplayName }}
                     span(v-if="modalImageMsg.Author === 'client'") {{ modalImageMsg.Client.Name }}
                     span.modal_img_header-title_date {{ modalImageMsg.CreatedAt | humanDateTime }}
-                .modal_img_header-icon
-                    svg(@click="downloadModalImg(modalImageMsg)" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512")
+                a(:href="modalImageMsg.File.URL", target="_blank", @click="clickFile(modalImageMsg, $event)")
+                  .modal_img_header-icon
+                    svg(xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512")
                       path(d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z")
 
             .modal_img
@@ -511,16 +512,16 @@
                       div(v-else-if="replyMsg.File && replyMsg.File.Type == 'image'")
                         a.image(
                           v-if="docWidth > 1024",
-                          v-bind:href="msg.File.URL",
+                          v-bind:href="replyMsg.File.URL",
                           target="_blank",
-                          @click="clickFile(msg, $event)"
+                          @click="clickFile(replyMsg, $event)"
                         )
-                          img.bubble(:src="msg.File.ThumbnailURL", :class="{ first: index === 0, last: index === group.Messages.length - 1 }")
+                          img.bubble(:src="replyMsg.File.ThumbnailURL", :class="{ first: index === 0, last: index === group.Messages.length - 1 }")
                         .image(
                           v-else-if="docWidth <= 1024",
-                          @click="clickFileImage(msg, $event)"
+                          @click="clickFileImage(replyMsg, $event)"
                         )
-                          img.bubble(:src="msg.File.ThumbnailURL", :class="{ first: index === 0, last: index === group.Messages.length - 1 }")
+                          img.bubble(:src="replyMsg.File.ThumbnailURL", :class="{ first: index === 0, last: index === group.Messages.length - 1 }")
                         button.img-button(v-if="replyMsg.Payload === 'carousel' || replyMsg.Payload === 'card'",
                           v-for="action of replyMsg.Actions") {{ action.Title }}
                       a.message_file(v-else-if="replyMsg.File && replyMsg.File.Type == 'file'"
