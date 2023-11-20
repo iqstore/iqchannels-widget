a88eee40bd4eba0e609640687dcaef26999c6d47
Pavelavl <sfdrus2004@gmail.com>
2023-11-20 11:06:39 +0300

    poll question 'one_of_list' edited

--- src/app/messenger/rating.vue
+++ src/app/messenger/rating.vue
@@ -189,6 +189,10 @@
   margin-top: 20px;
 }
 
+.m-b {
+  margin: 0 10px 10px 10px !important;
+}
+
 .poll_text {
   width: 85%;
   border: 1px solid #C8C7CC;
@@ -224,6 +228,7 @@
   flex-flow: column;
   .button {
     width: inherit;
+    margin: 0;
   }
 }
 
@@ -301,7 +306,7 @@
                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512")
                         path(fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM405.8 317.9l27.8 162L288 403.5 142.5 480l27.8-162L52.5 203.1l162.7-23.6L288 32l72.8 147.5 162.7 23.6-117.7 114.8z")
             .one_of_list.mt(v-if="poll.Questions[index].Type === 'one_of_list'")
-                .buttons-one-of-list.mt(v-for="(answer, i) in poll.Questions[index].Answers",
+                .buttons-one-of-list.m-b(v-for="(answer, i) in poll.Questions[index].Answers",
                     @click.prevent="setPollVariant(answer)"
                 )
                     .button(@click.prevent="setPollVariant(answer)")
@@ -398,7 +403,6 @@ export default {
                 ClientId: this.client.Id,
                 ProjectId: this.client.ProjectId,
             };
-            console.log(this.pollResult);
             this.sendRatingPoll();
         },
 
