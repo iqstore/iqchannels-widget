156f94da1880b49e443bd0be67f66282a555211f
pavelavl <sfdrus2004@gmail.com>
2023-12-24 23:07:34 +0300

    migration completed;

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,6 +1,7 @@
+const path = require('path');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const commonConfig = require('./common.js');
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 const CompressionPlugin = require('compression-webpack-plugin');
@@ -10,14 +11,14 @@ module.exports = webpackMerge.merge(commonConfig, {
   mode: 'production',
   // debug: false,
   output: {
-    path: 'build/',
+    path: path.join(__dirname, 'build/'),
     publicPath: '/widget/',
     filename: '[name].min.js',
     sourceMapFilename: '[name].js.map'
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.min.js'
+      'vue$': 'vue/dist/vue.cjs.js'
     }
   },
   plugins: [
@@ -26,13 +27,13 @@ module.exports = webpackMerge.merge(commonConfig, {
         'ENV': JSON.stringify(ENV)
       }
     }),
-    new ExtractTextPlugin('[name].min.css'),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-        screw_ie8: true /* eslint camelcase: 0 */
-      }
-    }),
+    // new ExtractTextPlugin('[name].min.css'),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compressor: {
+    //     warnings: false,
+    //     screw_ie8: true /* eslint camelcase: 0 */
+    //   }
+    // }),
     new CompressionPlugin()
   ]
 });
