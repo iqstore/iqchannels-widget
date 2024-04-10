98731314babcf4ca10df2d5477ba540cfb64fb8c
Pavelavl <sfdrus2004@gmail.com>
2024-04-10 10:29:09 +0300

    IQSD-1607: fixes

--- src/examples/0.js
+++ src/examples/0.js
@@ -1,8 +1,9 @@
 import "normalize.css";
+import { LOCALSTORAGE_CHANNEL_NAME } from "../schema";
 
 /* globals IQChannelsWidget */
 window.widget = new IQChannelsWidget({
-  channel: 'support',
+  channel: localStorage.getItem(LOCALSTORAGE_CHANNEL_NAME) || 'support',
   width: 425,
   requireName: false,
   iconOptions: { show: true }
