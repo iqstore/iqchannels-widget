09e437e79889a5b5b1aae09887f1fb2fc5c82a82
Pavelavl <sfdrus2004@gmail.com>
2024-06-10 02:03:46 +0300

    IQSD-1891: Распознавание номеров телефонов

--- src/app/messenger/message-text.vue
+++ src/app/messenger/message-text.vue
@@ -4,15 +4,35 @@
 <script>
 import linkifyString from "linkify-string";
 import markdownit from "markdown-it";
+import { findPhoneNumbersInText } from 'libphonenumber-js';
 
 export default {
   props: {
     msg: Object
   },
+
+  data: () => ({
+    getPhoneNum: [],
+  }),
+
   methods: {
     linkifyText(text) {
-      return linkifyString(text, {target: '_blank'});
+      return linkifyString(text, { target: '_blank' });
     },
+
+    telify(origText) {
+      let newText = origText;
+
+      if (newText?.length) {
+        this.getPhoneNum.forEach((match) => {
+          const pattern = origText.substring(match.startsAt, match.endsAt);
+          newText = newText.replace(pattern, `<a href="tel:${match.number.number}")>${pattern}</a>`);
+
+        });
+      }
+      return newText;
+    },
+
     transformMarkdown(text) {
       const md = markdownit({
         html: true,
@@ -21,17 +41,22 @@ export default {
       })
       return md.render(text);
     },
+
     displayText() {
-      switch (this.msg.Style) {
-        case "html":
+      this.getPhoneNum = findPhoneNumbersInText(this.msg.Text);
+      switch (true) {
+        case this.msg.Style === "html":
           return this.msg.Text;
-        case "markdown":
+        case this.msg.Style === "markdown":
           return this.transformMarkdown(this.msg.Text);
+        case this.getPhoneNum.length > 0:
+          return this.telify(this.msg.Text);
         default:
-          return this.linkifyText(this.msg.Text)
+          return this.linkifyText(this.msg.Text);
       }
     },
-    scrollToMessage(msg,event) {
+
+    scrollToMessage(msg, event) {
       this.$emit("scroll-to-message", msg, event);
     },
   }
