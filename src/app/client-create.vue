f4d6c60e11a04da3a3cfffc5de8bce5525ee679f
aakoroleva <a.koroleva@iqstore.ru>
2024-07-16 22:51:25 +0300

    merge master

--- src/app/client-create.vue
+++ src/app/client-create.vue
@@ -69,7 +69,7 @@ input[type="text"] {
 </style>
 
 <template lang="pug">
-.full-screen
+.full-screen#client-create
     div.close-button(href="#" @click.prevent="onCloseClicked()" title="Закрыть")
         svg(xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512")
             path(d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z")
@@ -77,31 +77,31 @@ input[type="text"] {
         p.text Подключение
             scale-loader(color="#ee5c13")
 
-    .center(v-else)
-        div(v-if="personalDataForm")
-            inforequest(
-                :request="personalDataForm",
-                :disableIgnore="true",
-                @send-info="sendInfo",
-            )
-        div(v-if="!personalDataForm")
-            div.default-form-wrapper
-                p.text
-                    strong {{ this.greetings?.GreetingBold  ? this.greetings.GreetingBold : "Представьтесь, пожалуйста,"  }}
-                    br
-                    span {{ this.greetings?.Greeting ? this.greetings.Greeting : "желательно указать" }}
-                    br
-                    span {{ !this.greetings?.Greeting ? "фамилию и имя:" : "" }}
-                input(type="text" placeholder="Ваше имя" ref="name" :disabled="creating" @keydown.enter="create" v-model="clientName")
-                div.client-consent
-                    input(type="checkbox" v-model="personalDataConsent").checkbox-custom
-                    span Согласие на обработку
-                        a(style="text-decoration:underline" :href="processDataLink" target="_blank") &nbsp;персональных данных
-                button.button(:disabled="!personalDataConsent || !clientName || clientName === ''" @click.prevent="create" href="#") 
-                    span(v-if="!creating") Начать чат
-                    scale-loader.loader(v-if="creating", color="#fff", height="12px")
+.center(v-else)
+    div(v-if="personalDataForm")
+        inforequest(
+            :request="personalDataForm",
+            :disableIgnore="true",
+            @send-info="sendInfo",
+        )
+    div(v-if="!personalDataForm")
+        div.default-form-wrapper
+            p.text
+                strong {{ this.greetings?.GreetingBold  ? this.greetings.GreetingBold : "Представьтесь, пожалуйста,"  }}
+                br
+                span {{ this.greetings?.Greeting ? this.greetings.Greeting : "желательно указать" }}
+                br
+                span {{ !this.greetings?.Greeting ? "фамилию и имя:" : "" }}
+            input(type="text" placeholder="Ваше имя" ref="name" :disabled="creating" @keydown.enter="create" v-model="clientName")
+            div.client-consent
+                input(type="checkbox" v-model="personalDataConsent").checkbox-custom
+                span Согласие на обработку
+                    a(style="text-decoration:underline" :href="processDataLink" target="_blank") &nbsp;персональных данных
+            button.button(:disabled="!personalDataConsent || !clientName || clientName === ''" @click.prevent="create" href="#") 
+                span(v-if="!creating") Начать чат
+                scale-loader.loader(v-if="creating", color="#fff", height="12px")
 
-                p.error &nbsp;{{ error }}
+            p.error &nbsp;{{ error }}
 
 
 </template>
