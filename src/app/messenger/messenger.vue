f67a18ca2ea9f7382dfce04e6ebc199623b7ebab
aakoroleva <a.koroleva@iqstore.ru>
2024-10-17 15:57:32 +0300

    drop logs

--- src/app/messenger/messenger.vue
+++ src/app/messenger/messenger.vue
@@ -193,8 +193,6 @@ export default {
             }
             const observer = new MutationObserver(() => {
                 const messageElement = document.getElementById('message-' + scrollToMessageId)
-                console.log({ messageElement });
-
                 if (messageElement) {
                     observer.disconnect()
                     messageElement.scrollIntoView({
@@ -411,7 +409,6 @@ export default {
                 ReplyToMessageId: messageForm.ReplyToMessageId
             });
             this.firstUnreadMessageId = null;
-            console.log("appendLocalMessage", message.Id, { scrollToMessage });
 
             this.appendMessage(message, scrollToMessage);
             return message;
