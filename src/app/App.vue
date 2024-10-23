a8c419f8f2dd533f7cc07cea1925f806e812ca15
Pavelavl <sfdrus2004@gmail.com>
2024-10-23 13:29:36 +0300

    PersonalDataRequestType = 'none' fix

--- src/app/App.vue
+++ src/app/App.vue
@@ -231,13 +231,18 @@ export default {
                     Greeting: res.Data?.Greeting,
                     GreetingBold: res.Data?.GreetingBold
                 };
-                if (res.Data?.PersonalDataRequestType === 'none') {
-                    this.requireName = false;
-                }
-                if (res.Data?.PersonalDataRequestType === 'full_form') {
-                    this.getPersonalDataForm()
-                } else {
-                    this.personalDataFormReady = true;
+                switch (res.Data?.PersonalDataRequestType) {
+                    case 'none':
+                        this.personalDataFormReady = true;
+                        this.requireName = false;
+                        break;
+                    case 'default':
+                        this.personalDataFormReady = true;
+                        this.requireName = true;
+                        break;
+                    default:
+                        this.requireName = true;
+                        this.getPersonalDataForm()
                 }
             });
         },
