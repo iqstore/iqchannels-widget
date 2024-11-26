a1087a48d5c9786a84cebd7aad043e11f7b26db2
Pavelavl <sfdrus2004@gmail.com>
2024-11-26 21:48:12 +0300

    IQSD-2734: fix

--- src/app/client-create.vue
+++ src/app/client-create.vue
@@ -115,7 +115,7 @@ export default {
                         input#personal-data-consent(type="checkbox" v-model="personalDataConsent").checkbox-custom
                         label(for="personal-data-consent") Согласие на обработку
                             a(style="text-decoration:underline" :href="processDataLink" target="_blank") &nbsp;персональных данных
-                    button.button(:disabled="!personalDataConsent || (requireName && !clientName?.trim())" @click.prevent="create" href="#")
+                    button.button(:disabled="!personalDataConsent || requireName && !clientName?.trim()" @click.prevent="create" href="#")
                         span(v-if="!creating") Начать чат
                         scale-loader.loader(v-if="creating", color="#fff", height="12px")
 
