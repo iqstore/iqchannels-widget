8fbda23bef3cc1f18e64e149f47fb2a3c8cee9d0
aakoroleva <a.koroleva@iqstore.ru>
2024-12-04 16:15:39 +0300

    Revert "removed files config"
    This reverts commit beeccece0943384eff725772ae9243e571ecefee.

--- src/app/App.vue
+++ src/app/App.vue
@@ -112,6 +112,9 @@ export default {
                     client.version().then(version => {
                         client.iQVersion = version?.Data?.Version;
                     });
+                    client.filesConfig().then(resp => {
+                        client.configFiles = resp?.Result;
+                    });
                     break;
 
                 case 'close':
