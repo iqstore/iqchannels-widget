ea56108308a1cc47221fc5a3e57e494d8aaa7ee8
aakoroleva <a.koroleva@iqstore.ru>
2025-01-27 10:46:52 +0300

    IQSD-2952: Добавлено accep, capture в инпут файла

--- src/app/messenger/composer.vue
+++ src/app/messenger/composer.vue
@@ -562,7 +562,7 @@ export default {
                 upload-file(:current-file="file", @reset-upload="resetUploadFile")
 
         .composer-inputs(:class="{'disabled': disableFreeText}")
-            input.hidden(type="file" ref="uploadInput" @change="uploadFile" multiple)
+            input.hidden(type="file" accept="image/*" capture ref="uploadInput" @change="uploadFile" multiple)
             a.button.csm-btn(id="record-cancel" v-if="recording || recordingStopped" @click="cancelRecording()", style="cursor:pointer")
                 svg(xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512")
                     path(d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z")
