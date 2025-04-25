40e93fb15be8e7f9e465c50bf4a900c2a3b2bb38
aakoroleva <a.koroleva@iqstore.ru>
2025-04-25 11:21:53 +0300

    test: kazan config

--- src/examples/0.js
+++ src/examples/0.js
@@ -2,9 +2,9 @@ import "normalize.css";
 import { DefaultAnonClientConfig, LOCALSTORAGE_WIDGET_0_CONFIG } from "../schema";
 import { configureWidget } from "./examples-utils";
 
-window.configureWidget = configureWidget.bind(null, LOCALSTORAGE_WIDGET_0_CONFIG, DefaultAnonClientConfig);
+window.configureWidget = configureWidget.bind(null, DefaultAnonClientConfig);
 window.onload = () => {
-	let config = JSON.parse(window.localStorage.getItem(LOCALSTORAGE_WIDGET_0_CONFIG)) ?? DefaultAnonClientConfig;
+	let config = JSON.parse(DefaultAnonClientConfig);
 	const current = window.document.getElementById("current_config");
 	current.innerHTML = config ? `${JSON.stringify(config, null, 2)}` : `Нету конфигурации.`;
 };
