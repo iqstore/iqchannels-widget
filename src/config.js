ccb7fe2b99918e974a55b690d0b87e17fc8873e1
zhuckovkj <zhuckovkj@gmail.com>
2025-09-12 16:16:48 +0300

    fix image auth

--- src/config.js
+++ src/config.js
@@ -1,21 +1,30 @@
 class Config {
-
   REQUEST_MESSAGES_LIMIT = 50;
-  CLIENT_SESSION_COOKIE = 'client-session';
+  CLIENT_SESSION_COOKIE = "client-session";
 
-  constructor () {
+  constructor() {
     // this.apiBaseUrl = 'https://iqchannels.isimplelab.com/public/api/';
-    this.apiBaseUrl = '/public/api/';
-    this.apiVersion = 'v1';
+    this.apiBaseUrl = "/public/api/";
+    this.apiVersion = "v1";
+  }
+
+  getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+    return null;
   }
 
   apiUrl = (path) => this.apiBaseUrl + this.apiVersion + path;
 
   fileUrl = (fileId) => this.apiUrl(`/files/get/${fileId}`);
 
-  imageUrl = (imageId, size) => this.apiUrl(`/files/image/${imageId}?size=${size}`);
-
+  imageUrl = (imageId, size) => {
+    const token = this.getCookie(this.CLIENT_SESSION_COOKIE);
+    
+    const tokenParam = token ? `&x-client-token=${encodeURIComponent(token)}` : '';
+    return this.apiUrl(`/files/image/${imageId}?size=${size}${tokenParam}`);
+  };
 }
 
 export default new Config();
-
